70a5fe4a44396a5016522cdeb6b2df23bb6ee919
mark <41400439+markanthonybanong@users.noreply.github.com>
2021-06-18 23:16:28 +0800

    Update send-message.js

--- src/socket/send-message.js
+++ src/socket/send-message.js
@@ -1,6 +1,7 @@
 
 module.exports = function(socket){
-    socket.on('send-message', (message, room, lastInsertedMsgId)=>{
+    socket.on('send-message', (message, room, lastInsertedMsgId, receiverWpUserId)=>{
         socket.to(room).emit('receive-message', message, lastInsertedMsgId);
+        socket.broadcast.emit('notification-message', receiverWpUserId);
     });
 };
